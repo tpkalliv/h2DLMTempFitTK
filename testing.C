e8b642598cf7fbffd86bf3a2176b49484c12135c
Teemu Kallio <you@example.com>
2022-08-25 13:23:52 +0300

    25.8 push

---
+++ testing.C
@@ -0,0 +1,4 @@
+// I should have a list of parameters -> h2dLMTempFit saves parameters to a .txt file on rows (column names: ic, pt, v2, v2 err, v3, v3 err)
+
+// GOAL: Program outputs a fig which has on x-axis one parameter and y-axis some other which are from the .txt file
+
